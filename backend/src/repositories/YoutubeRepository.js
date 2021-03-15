0f7ed9ca7faa9d658758e65842b9d9bc3c2f2dcf
Timmy_DESK <timmy85618@gmail.com>
2021-03-15 20:52:37 +0800

    API修正

--- backend/src/repositories/YoutubeRepository.js
+++ backend/src/repositories/YoutubeRepository.js
@@ -5,17 +5,24 @@ exports.getChannelRecentInfo = async function (channelId) {
   return ytXmlDataProccess(data);
 }
 
-function ytXmlDataProccess(data){
+function ytXmlDataProccess(data) {
   return data.feed.entry.map(function (data) {
-    const { title, link, published } = data;
+    const { title, yt, published } = data;
+    let videoid = data['yt:videoId'][0];
     let re = /#(?<tag>\S+)/g;
     let tag = re.exec(data['media:group'][0]['media:description'][0])
+    if (tag === null)
+      tag = null;
+    else
+      tag = tag[0];
+    var dt = new Date(published[0]);
+    let date = dt.getFullYear() + "-" + dt.getMonth() + "-" + dt.getDate();
     return result = {
       title: title[0],
-      link: link[0].$.href,
-      date: published[0],
+      videoid: videoid,
+      date: date,
       imgurl: data['media:group'][0]['media:thumbnail'][0].$.url,
-      tag: tag[0]
+      tag: tag
     };
   })
 }
